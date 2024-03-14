9233ef843ff2493b4f97aa57c025e9e48e1a4a43
Robert Epps <robertjeppsiv@gmail.com>
2024-03-14 15:27:27 -0700

    Add TCC logo back, needs better styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,14 @@
 <!-- This component should prooobbably go away if under a certain screen width (mobile viewers) -->
 
 <script>
-
+    import logo from '$lib/images/tcc_grayscale.svg'
 </script>
 
 <header>
-    
+    <a href="/">
+        <img src={logo} alt="TCC Logo"/>
+    </a>
+
 </header>
 
 <style>
@@ -13,5 +16,9 @@
         width: 100%;
         height: 4em;
         background-color: rgba(10, 10, 10, 0.5);
+    }
+
+    img {
+        width: 10px;
     }
 </style>
