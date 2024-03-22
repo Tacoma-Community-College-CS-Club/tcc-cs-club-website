e55bbdad6cd927753038d09c628c401dac3baccc
mechanicalruby <robbierocket05@gmail.com>
2024-03-22 16:41:39 -0700

    Rounded profile pictures, start work on contact page

--- src/lib/components/ProfilePicture.svelte
+++ src/lib/components/ProfilePicture.svelte
@@ -1,18 +1,21 @@
 <script>
+    export let name = "Default Name"; // First, last name for initials if image is unavailable or cannot be loaded
     export let src; // Image source prop
     export let alt; // Image alt text prop
 </script>
 
 <div class="picture-body">
-    <img src={src} alt="A profile picture">
+    <img src={src} alt="A profile picture" class="inner-picture">
 </div>
 
 <style>
     .picture-body {
         margin: 10px;
-        width: 150px;
-        height: 150px;
-        border-radius: 200px;
-        /* background-color: black; */
+        width: 8em;
+        height: 8em;
+    }
+
+    .inner-picture {
+        border-radius: 50%;
     }
 </style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,16 +1,51 @@
 <script>
     import ProfilePicture from "$lib/components/ProfilePicture.svelte";
-
-
+    import test_profile_picture from '$lib/images/profile_pictures/testpic.webp';
 </script>
 
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+    <title>About</title>
+    <meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>The club </h1>
-    <ProfilePicture src='$lib/images/profile_pictures/testpic.webp'/>
+<div class="container">
+    <div class="contact-container">
+        <div class="profile-picture">
+            <ProfilePicture src={test_profile_picture}/>
+        </div>
+        <ul class="contact-info">
+            <li><strong>John Smith</strong></li>
+            <li><i>Club President</i></li>
+        </ul>
+    </div>
 
+    <div class="contact-container">
+        <div class="profile-picture">
+            <ProfilePicture src={test_profile_picture}/>
+        </div>
+        <ul class="contact-info">
+            <li><strong>John Smith</strong></li>
+            <li><i>Vice President</i></li>
+        </ul>
+    </div>
 </div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column; /* Stack items vertically */
+        justify-content: top; /* Horizontally center contents */
+        align-items: center; /* Vertically center contents */
+        height: 100vh; /* Set height to full viewport height */
+    }
+
+    .contact-container {
+        display: flex;
+        align-items: center; /* Vertically align items */
+        margin-bottom: 20px; /* Adjust spacing between contact containers */
+    }
+
+    .profile-picture {
+        margin-right: 10px; /* Adjust spacing between profile picture and info */
+    }
+</style>
