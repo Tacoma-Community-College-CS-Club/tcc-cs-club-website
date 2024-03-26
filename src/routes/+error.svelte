1e4d8a0be2ce30c7dd9f1bc272896f481befeb68
mechanicalruby <robbierocket05@gmail.com>
2024-03-26 09:54:47 -0700

    Add an error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<div class="page-content">
+    <h1>404</h1>
+    <p>You gazed into the abyss, and the abyss said "Page Not Found".</p>
+</div>
+
+<style>
+    .page-content {
+        margin: auto;
+        text-align: center;
+    }
+
+    h1 {
+        font-family: "DM Sans";
+        font-weight: bold;
+        font-size: 3em;
+        line-height: 1em;
+    }
+</style>
